8368646316fdf465fa912297b984dbd4c3075606
Vasily Mikryukov <vasmik@gmail.com>
2018-07-09 01:33:04 -0700

    fix fake cache implementation

--- app/cache/hashmap.go
+++ app/cache/hashmap.go
@@ -3,20 +3,20 @@ package cache
 // NewHashMapCache returns in-memory hashmap cache implementation. For development purposes
 func NewHashMapCache() (DataCache, error) {
 	return &fakeCache{
-		Storage: make(map[string][]byte, 0),
+		storage: make(map[string][]byte, 0),
 	}, nil
 }
 
 type fakeCache struct {
-	Storage map[string][]byte
+	storage map[string][]byte
 }
 
 func (c *fakeCache) Get(key string) (data []byte, err error) {
-	return c.Storage[key], nil
+	return c.storage[key], nil
 }
 
 func (c *fakeCache) Set(key string, data []byte) error {
-	c.Storage[key] = data
+	c.storage[key] = data
 	return nil
 }
 
