5a29638c699e24a6776f0459a712fa06e8d002da
Vasily Mikryukov <vasmik@gmail.com>
2018-07-18 20:15:53 -0700

    Fix scanner parameters

--- start-scanner.sh
+++ start-scanner.sh
@@ -7,5 +7,5 @@ sonar-scanner \
   -Dsonar.sources=app \
   -Dsonar.go.coverage.reportPaths=sonar-coverage.out \
   -Dsonar.go.tests.reportPaths=sonar-report.json \
-  -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.login=d73b99b735b80b0ab4bb46ba2a9c81d5f7e94afa
+  -Dsonar.host.url=http://localhost:9000
+  
